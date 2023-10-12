f88149b925f6db6a01494bd3a3e6088cdca3cd3d
Alan Reyes <kutt@katrea.net>
2023-10-11 23:04:42 -0600

    Fixes version check (was failing with Sublime Text 4)

--- diagram/sublime3.py
+++ diagram/sublime3.py
@@ -1,10 +1,20 @@
 from .base import BaseViewer
 import sublime
+import re
 
 class Sublime3Viewer(BaseViewer):
 	def load(self):
 		if not sublime.version().startswith('3'):
 			raise Exception("Not Sublime 3!")
+		version_match = re.match(r"\d+", sublime.version())
+
+		if not version_match:
+			raise Exception("Not Sublime 3! (%s)" % sublime.version())
+
+		numeric_version = int(version_match.group(0))
+
+		if numeric_version < 3000:
+			raise Exception("Not Sublime 3! (%s)" % sublime.version())
 
 	def view(self,diagram_files):
 		for diagram_file in diagram_files:
